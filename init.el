a14ac20482a58596afe850d6e1de3ac4d7786563
fqye <fqye@lanyusan.com>
2020-06-04 17:07:51 +0800

    changed logo to default

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 ;; Copyright (C) 2019 Mingde (Matthew) Zeng
 ;; Created: Thu Mar 14 10:15:28 2019 (-0400)
 ;; Version: 2.0.0
-;; Last-Updated: Thu Jun  4 16:40:41 2020 (+0800)
+;; Last-Updated: Thu Jun  4 17:05:43 2020 (+0800)
 ;;           By: fqye
 ;; URL: https://github.com/MatthewZMD/.emacs.d
 ;; Keywords: M-EMACS .emacs.d init
