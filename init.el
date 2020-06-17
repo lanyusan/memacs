23b3732b8e7b67ce27f93582dfa49bba13a28a1b
fqye <fqye@lanyusan.com>
2020-06-17 16:14:37 +0800

    added rest client mode

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 ;; Copyright (C) 2019 Mingde (Matthew) Zeng
 ;; Created: Thu Mar 14 10:15:28 2019 (-0400)
 ;; Version: 2.0.0
-;; Last-Updated: Thu Jun  4 17:05:43 2020 (+0800)
+;; Last-Updated: Tue Jun  9 10:55:45 2020 (+0800)
 ;;           By: fqye
 ;; URL: https://github.com/MatthewZMD/.emacs.d
 ;; Keywords: M-EMACS .emacs.d init
@@ -231,7 +231,7 @@ If you experience freezing, decrease this.  If you experience stuttering, increa
 
 (require 'init-zone)
 
-
+(require 'init-restclient)
 
 (provide 'init)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
